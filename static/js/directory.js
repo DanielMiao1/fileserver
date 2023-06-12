85d942355744c4006fb7e13bab1a25ae697845fa
Daniel M <danielmiao2019@icloud.com>
2023-06-12 15:55:38 -0700

    create file upload form in javascript

--- static/js/directory.js
+++ static/js/directory.js
@@ -242,4 +242,16 @@ export default function loadDirectory(items) {
 
 		container.appendChild(button);
 	}
+
+	const file_upload = document.createElement("input");
+	file_upload.type = "file";
+	file_upload.id = "file";
+	file_upload.name = "file";
+
+	const file_upload_wrapper = document.createElement("form");
+	file_upload_wrapper.method = "post";
+	file_upload_wrapper.enctype = "multipart/form-data";
+
+	file_upload_wrapper.appendChild(file_upload);
+	document.body.appendChild(file_upload_wrapper);
 }
