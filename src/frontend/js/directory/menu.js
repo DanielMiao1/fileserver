5bce2c6cb8f505f4448ad4f26b205b5f861ff3ad
Daniel Miao <danielmiao2019@icloud.com>
2024-06-17 14:03:05 -0700

    feat(frontend): implement file renaming GUI

--- src/frontend/js/directory/menu.js
+++ src/frontend/js/directory/menu.js
@@ -1,17 +1,54 @@
+function createRenameInput(target) {
+	const old_filename = target.children[0].innerText;
+
+	const input_element = document.createElement("input");
+	input_element.value = old_filename;
+	target.children[0].remove();
+	target.appendChild(input_element);
+
+	input_element.focus();
+	input_element.selectionStart = old_filename.length;
+
+	input_element.addEventListener("keypress", event => {
+		if (event.key === "Enter") {
+			let enclosing_directory = document.location.pathname;
+			if (!enclosing_directory.endsWith("/")) {
+				enclosing_directory += "/";
+			}
+
+			const old_path = enclosing_directory.slice(5) + old_filename;
+			const new_path = enclosing_directory + input_element.value;
+
+			fetch(new_path, {
+				method: "put",
+				headers: {
+					path: old_path
+				}
+			}).then(response => {
+				if (response.ok) {
+					document.location.reload();
+				}
+			})
+		}
+	})
+}
+
 export function menuHandler(event) {
 	return {
-		Delete: () => fetch(`${(
-			document.location.pathname.endsWith("/") ?
-			document.location.pathname :
-			`${document.location.pathname}/`
-		)}${event.target.title}`, {
-			method: "DELETE"
-		}
-	).then(response => {
-		if (response.ok) {
-			document.location.reload();
-		}
-	}),
+		Delete: () => fetch(
+			`${(
+				document.location.pathname.endsWith("/") ?
+				document.location.pathname :
+				`${document.location.pathname}/`
+			)}${event.target.title}`,
+			{
+				method: "DELETE"
+			}
+		).then(response => {
+			if (response.ok) {
+				document.location.reload();
+			}
+		}),
 		Download: () => {
 			document.getElementById("downloader").src = `/download${(
 				document.location.pathname.endsWith("/") ?
@@ -21,6 +58,6 @@ export function menuHandler(event) {
 		},
 		// TODO: Properly open the file
 		Open: () => event.target.dispatchEvent(new MouseEvent("dblclick")),
-		Rename: () => event.target.click()
+		Rename: () => createRenameInput(event.target)
 	}
 }
