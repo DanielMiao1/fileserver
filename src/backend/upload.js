b8d293483d92fa2a5b9d20cc8de54e76c43fde86
Daniel Miao <danielmiao2019@icloud.com>
2024-06-17 21:15:28 -0700

    feat(backend,upload): implement filename deduplication for uploads

--- src/backend/upload.js
+++ src/backend/upload.js
@@ -20,17 +20,47 @@ export function registerMultipartHooks(server) {
 	});
 }
 
+function fileNameAndExtension(path) {
+	const dot_index = path.lastIndexOf(".");
+	const slash_index = path.lastIndexOf("/");
+
+	if (dot_index === -1) {
+		return [path, ""];
+	}
+
+	if (slash_index !== -1 && dot_index < slash_index) {
+		return [path, ""];
+	}
+
+	return [path.slice(0, dot_index), path.slice(dot_index)];
+}
+
+function assignDeduplicateFilename(path) {
+	let deduplicated_path = path;
+
+	while (fs.existsSync(deduplicated_path)) {
+		const [filename, extension] = fileNameAndExtension(deduplicated_path);
+		if (filename.endsWith(" copy")) {
+			deduplicated_path = filename + " 1" + extension;
+		} else if (/ copy \d+$/u.test(filename)) {
+			const copy_index = filename.lastIndexOf(" ") + 1;
+			const new_digit = parseInt(filename.slice(copy_index)) + 1;
+			deduplicated_path = filename.slice(0, copy_index) + new_digit + extension;
+		} else {
+			deduplicated_path = filename + " copy" + extension;
+		}
+	}
+
+	return deduplicated_path;
+}
+
 export function registerUploadHooks(server) {
 	server.post("/*", async (request, reply) => {
 		const path = serving_directory + request.url;
 	
 		for await (const part of request.files()) {
-			const file_path = `${path}/${part.filename}`;
-			
-			if (fs.existsSync(file_path)) {
-				return reply.status(409).send();
-			}
-	
+			const file_path = assignDeduplicateFilename(path + part.filename);
+
 			upload(part.file, fs.createWriteStream(file_path));
 		}
 	
