52368899216aeb4ed62168a86b7a79cd29b00871
DanielMiao1 <danielmiao2019@icloud.com>
2024-08-01 04:43:35 -0700

    feat(fileicon): RTF and TXT icons, formatting

--- src/frontend/js/directory/entry.js
+++ src/frontend/js/directory/entry.js
@@ -23,7 +23,7 @@ function selectItem(button, element) {
 }
 
 function assignFileIcon(filename) {
-	for (const extension of ["jpeg", "jpg", "png", "svg"]) {
+	for (const extension of ["jpeg", "jpg", "png", "rtf", "svg", "txt"]) {
 		if (filename.endsWith(`.${extension}`)) {
 			return `/static/img/extensions/${extension}.svg`;
 		}
